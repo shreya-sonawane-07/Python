dcea79d7025fe4a04b855845436da9e296596161
shreya-sonawane-07 <shreyasonawane662@gmail.com>
2025-06-19 21:59:25 +0530

    Add files via upload

---
+++ maximum3.py
@@ -0,0 +1,12 @@
+x=int(input("Enter 1st number: "))
+y=int(input("Enter 2nd number: "))
+z=int(input("Enter 3rd number: "))
+
+if x>y and x>z:
+    print(x,"is Greater")
+elif y>x and y>z:
+    print(y,"is Greater")
+elif z>x and z>y:
+    print(z,"is Greater")
+
+    

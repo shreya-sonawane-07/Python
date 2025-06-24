9d60b29377b804b54246b33ad18d510dcdd340df
shreya-sonawane-07 <shreyasonawane662@gmail.com>
2025-06-24 14:37:43 +0530

    Add files via upload

---
+++ practic.py
@@ -0,0 +1,9 @@
+x=int(input("Enter the first number: "))
+y=int(input("Enter the second number: "))
+z=int(input("Enter the thired number: "))
+if x>y and x>z:
+    print(x,"the number is maximum")
+elif y>x and y>z:
+    print(y,"the number is maximum")
+elif z>x and z>y:
+    print(z,"the number is maximum")
